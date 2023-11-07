17b9143e16f027b7a0c68c79583db6995e737522
santinix <clayton_santinif@hotmail.com>
2023-11-07 19:51:21 -0300

    Funções criadas

--- calc.js
+++ calc.js
@@ -1,3 +1,34 @@
 let elementoInputUm = document.getElementById("inputUm")
 let elementoInputDois = document.getElementById("inputDois")
 let totalgit
+
+function somar(){
+    
+    total =  Number(elementoInputUm.value) + Number(elementoInputDois.value)
+
+    alert(`O total da soma é de ${total}`)
+}
+
+
+function subtrair(){
+    
+    total =  Number(elementoInputUm.value) - Number(elementoInputDois.value)
+
+    alert(`O total da subtração é de ${total}`)
+}
+
+
+function divisao(){
+    
+    total =  Number(elementoInputUm.value) / Number(elementoInputDois.value)
+
+    alert(`O total da divisão é de ${total}`)
+}
+
+
+function multiplica(){
+    
+    total =  Number(elementoInputUm.value) * Number(elementoInputDois.value)
+
+    alert(`O total da multiplicação é de ${total}`)
+}
